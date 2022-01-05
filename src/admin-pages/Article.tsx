d01ed391235dd84e821a1abb942a08f903cc7d60
guozhao <davidkoojohn@163.com>
2022-01-05 23:53:00 +0800

    add page header

--- src/admin-pages/Article.tsx
+++ src/admin-pages/Article.tsx
@@ -1,9 +1,28 @@
+import { CSSProperties } from "react"
 import { Outlet } from "react-router-dom"
+import { PageHeader, Button } from "antd"
+
+import wbb from "@/assets/wbb.jpeg"
+
+const styled: Record<string, CSSProperties> = {
+  bordered: {
+    border: "1px solid rgb(235, 237, 240)",
+    backgroundColor: "#fff"
+  }
+}
 
 export default function Article() {
   return (
     <>
-      <h1>Admin Article</h1>
+      <PageHeader
+        style={styled.bordered}
+        avatar={{ src: wbb }}
+        title="文章管理"
+        subTitle="This is a subtitle"
+        extra={
+          <Button>新建文章</Button>
+        }
+      />
       <Outlet/>
     </>
   )
--- src/admin-pages/article-pages/Index.tsx
+++ src/admin-pages/article-pages/Index.tsx
@@ -1,5 +1,43 @@
+import { Table } from "antd"
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
 export default function ArticleIndex() {
   return (
-    <h1>Admin Article Index</h1>
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+    />
   )
 }
